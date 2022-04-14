8dbc34ca66e126d9554cee7bfd801b5d1f7ada98
amalia <amalia.avramov@yahoo.com>
2022-04-14 15:31:12 +0300

    Added my project

---
+++ app/src/main/java/com/doubletex/app/CatsAPI.java
@@ -0,0 +1,16 @@
+package com.doubletex.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class CatsAPI {
+    @GetMapping("cats")
+    public String getCats(
+            @RequestParam("breed") String breed){
+        return "There was a cat named Mittens who was " + breed;
+    }
+}
