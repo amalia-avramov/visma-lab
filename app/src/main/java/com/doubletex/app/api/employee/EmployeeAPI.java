af7200cf5e98728855a320cc5f6d953b979bd6b4
Amalia Avramov <amalia.avramov@yahoo.com>
2022-05-09 09:46:42 +0300

    Lab5 - Validations

--- app/src/main/java/com/doubletex/app/api/employee/EmployeeAPI.java
+++ app/src/main/java/com/doubletex/app/api/employee/EmployeeAPI.java
@@ -1,8 +1,10 @@
 package com.doubletex.app.api.employee;
 
+import com.doubletex.app.errors.DbtNotFound;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
@@ -12,12 +14,12 @@ public class EmployeeAPI {
     private final EmployeeService employeeService;
 
     @GetMapping("/{id}")
-    public Optional<Employee> get(@PathVariable Long id){
-        return employeeService.get(id);
+    public Optional<Employee> get (@PathVariable Long id){
+        return employeeService.get(id) ;
     }
 
     @PostMapping("")
-    public void post(@RequestBody Employee employee){
+    public void post(@Valid @RequestBody Employee employee){
         employeeService.post(employee);
     }
 }
